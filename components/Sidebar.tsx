b245ee27254c347e75840796b8b47b9a476f685b
Prebenww <63924500+Prebenww@users.noreply.github.com>
2021-12-30 16:53:57 +0100

    bug fix

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,8 +1,7 @@
-
-import {AiFillGithub} from  "react-icons/ai"
-import {AiOutlineCloudDownload} from  "react-icons/ai"
-import {AiFillLinkedin} from  "react-icons/ai"
-import {GoLocation} from  "react-icons/go"
+import {AiFillGithub} from "react-icons/ai"
+import {AiOutlineCloudDownload} from "react-icons/ai"
+import {AiFillLinkedin} from "react-icons/ai"
+import {GoLocation} from "react-icons/go"
 import {GiTie} from "react-icons/gi"
 import {SiExpo} from "react-icons/si"
 import {useTheme} from "next-themes"
@@ -12,7 +11,7 @@ import Image from "next/image"
 
 const Sidebar = () => {
 
-    const {theme, setTheme} =  useTheme();
+    const {theme, setTheme} = useTheme();
 
 
     const changeTheme = () => {
@@ -28,33 +27,34 @@ const Sidebar = () => {
                 className="w-32 h-32 mx-auto rounded-full"
 
 
-
             />
             <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
                 <span className="text-green">Preben </span>
                 Wulff</h3>
-            <p  className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500"
+            <p className="px-2 py-1 my-3 bg-gray-200 rounded-full dark:bg-dark-200 dark:bg-black-500"
 
             >Webutvikler</p>
             <a
                 className="flex items-center justify-center px-2 py-1 my-2 bg-gray-200 rounded-full cursor-pointer dark:bg-dark-200 dark:bg-black-500"
                 href="CV_PrebenWulff.pdf"
-                download="CV_PrebenWulff.pdf"><AiOutlineCloudDownload className="w-6 h-6" style={{marginRight: 5}}/> Last ned CV</a>
+                download="CV_PrebenWulff.pdf"><AiOutlineCloudDownload className="w-6 h-6"
+                                                                      style={{marginRight: 5}}/> Last ned CV</a>
             {/*//social buttons*/}
-            <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full" >
+            <div className="flex justify-around w-9/12 mx-auto my-5 text-green md:w-full">
 
-                <a href="" ><AiFillGithub className="w-8 h-8 cursor-pointer" aria-label="Github"/></a>
+                <a href=""><AiFillGithub className="w-8 h-8 cursor-pointer" aria-label="Github"/></a>
 
-                <a href="" ><AiFillLinkedin className="w-8 h-8 cursor-pointer" aria-label="Linkedin"/></a>
+                <a href=""><AiFillLinkedin className="w-8 h-8 cursor-pointer" aria-label="Linkedin"/></a>
 
-                <a href="" ><SiExpo className="w-7 h-8 cursor-pointer" aria-label="expo"/></a>
+                <a href=""><SiExpo className="w-7 h-8 cursor-pointer" aria-label="expo"/></a>
 
             </div>
             {/*address*/}
-            <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500" style={{marginLeft: "-1rem", marginRight: "-1rem"}}>
+            <div className="py-4 my-5 bg-gray-200 dark:bg-dark-200 dark:bg-black-500"
+                 style={{marginLeft: "-1rem", marginRight: "-1rem"}}>
                 <div className="flex items-center justify-center space-x-2">
-                    <GoLocation  />
-                <span>
+                    <GoLocation/>
+                    <span>
                     Bergen, Norge
                 </span>
                 </div>
@@ -70,7 +70,8 @@ const Sidebar = () => {
             ">
                 Kontakt meg
             </button>
-            <button onClick={changeTheme} className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">
+            <button onClick={changeTheme}
+                    className="bg-gradient-to-r from-green to-blue-400 w-8/12 rounded-full py-2 px-5 text-white my-2">
                 {theme === "light" ? "Dark Mode" : "Light Mode"}
             </button>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import {services} from "../data";
-import {GetServerSidePropsContext, GetStaticPropsContext, } from "next";
+import {GetServerSidePropsContext, GetStaticPropsContext,} from "next";
 import ServiceCard from "../components/ServiceCard";
 import {motion} from "framer-motion";
 import {fadeInUp} from "../animations";
@@ -10,28 +10,30 @@ import Head from "next/head"
 
 const index = () => {
 
-    return(
+    return (
         <motion.div
-           variants={routeAnimation} initial="initial" animate="animate" exit="exit"
+            variants={routeAnimation} initial="initial" animate="animate" exit="exit"
             className="flex flex-col flex-grow px-6 pt-1 "
         >
             <Head>
                 <title>Web utvikler | portfolio | Preben Wulff</title>
             </Head>
 
-            <h6 className="my-3 text-base font-medium"> Teknologi entusiast og entreprenør. Studerer Informasjonssystemer med fordypning i Software Engineering og Business lntelligence - ferdig utdannet våren 2022.
+            <h6 className="my-3 text-base font-medium"> Teknologi entusiast og entreprenør. Studerer
+                Informasjonssystemer med fordypning i Software Engineering og Business lntelligence - ferdig utdannet
+                våren 2022.
                 Har erfaring med HTML, ess og JS og frameworks som React JS, React Native.
                 Driver til daglig mediebedriften Mediatrafikk.
             </h6>
             <div className="flex-grow p-4 mt-5 bg-gray-400 dark:bg-dark-100 "
-                 style={{ marginLeft: "-1.5rem", marginRight: "-1.5rem" }}
+                 style={{marginLeft: "-1.5rem", marginRight: "-1.5rem"}}
             >
                 <h4 className="my-3 text-xl font-semibold tracking-wide">Ferdigheter</h4>
                 <motion.div
-                            variants={stagger}
-                            initial="initial"
-                            animate="animate"
-                            className="grid gap-6 my-3 md:grid-cols-2"
+                    variants={stagger}
+                    initial="initial"
+                    animate="animate"
+                    className="grid gap-6 my-3 md:grid-cols-2"
                 >
                     {
                         services.map(service => (
@@ -39,7 +41,7 @@ const index = () => {
                                 variants={fadeInUp}
                                 key={service.title}
                                 className="col-span-2 p-2 bg-gray-200 rounded-lg dark:bg-dark-200 md:col-span-1 ">
-                            <ServiceCard service={service}/>
+                                <ServiceCard service={service}/>
                             </motion.div>
                         ))}
                 </motion.div>
@@ -51,13 +53,13 @@ const index = () => {
 export default index;
 
 export const getServerSideProps = async (
-    context:GetServerSidePropsContext
+    context: GetServerSidePropsContext
 ) => {
 
-/*    const res = await fetch(`${process.env.VERCEL_URL_}/api/services`)
-    const data = await res.json();*/
+    /*    const res = await fetch(`${process.env.VERCEL_URL_}/api/services`)
+        const data = await res.json();*/
 
-    return {props:{ endpoint: process.env.VERCEL_URL }}
+    return {props: {endpoint: process.env.VERCEL_URL}}
 }
 
 /*export const getStaticProps = async (context:GetStaticPropsContext) => {
